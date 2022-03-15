4dc0273a96d0286a0f914428a34350e50344a3c8
Martin Jeanne <mjeanne@sio.local>
2022-03-15 12:12:52 +0100

    First commit

---
+++ data/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE album (
+id INTEGER PRIMARY KEY auto_increment, 
+artist varchar(100) NOT NULL, 
+title varchar(100) NOT NULL,
+img LONGBLOB NOT NULL
+);
+INSERT INTO album (artist, title, img) VALUES ('The Military Wives', 'In My Dreams', LOAD_FILE('C:\Users\mjeanne\Pictures\adele.png'));
+INSERT INTO album (artist, title, img) VALUES ('Adele', '21', LOAD_FILE('C:\Users\mjeanne\Pictures\adele.png'));
+INSERT INTO album (artist, title, img) VALUES ('Bruce Springsteen', 'Wrecking Ball (Deluxe)', LOAD_FILE('C:\Users\mjeanne\Pictures\adele.png'));
+INSERT INTO album (artist, title, img) VALUES ('Lana Del Rey', 'Born To Die', LOAD_FILE('C:\Users\mjeanne\Pictures\adele.png'));
+INSERT INTO album (artist, title, img) VALUES ('Gotye', 'Making Mirrors', LOAD_FILE('C:\Users\mjeanne\Pictures\adele.png'));
